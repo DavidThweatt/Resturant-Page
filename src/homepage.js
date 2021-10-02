e636eb06defd971d366fd666c47027abe5486d55
DavidThweatt <ddthweatt9@gmail.com>
2021-10-02 10:43:49 -0700

    first commit

---
+++ src/homepage.js
@@ -0,0 +1,12 @@
+//in header
+const header = document.createElement('div');
+const nav = document.createElement('div');
+const home_tab = document.createElement('div');
+const menu_tab = document.createElement('div');
+const contact_tab = document.createElement('div');
+
+// in content div
+const restaurant_name = document.createElement('H1');
+const info = document.createElement('div');
+const hours = document.createElement('div');
+const location = document.createElement('div');
